0a4cf4eac53a2f7eac451bf391768c38e8b52717
HardPlant <abc7988se@naver.com>
2018-11-27 00:13:09 +0900

    typo

--- securing_web/src/main/java/hello/MvcConfig.java
+++ securing_web/src/main/java/hello/MvcConfig.java
@@ -1,7 +1,7 @@
 package hello;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistery;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
