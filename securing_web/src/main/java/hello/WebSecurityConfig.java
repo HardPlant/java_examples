17945000d42e86f14a87845aa16b1481292c1689
HardPlant <abc7988se@naver.com>
2018-11-27 01:04:19 +0900

    메서드를 이용해 설정

--- securing_web/src/main/java/hello/WebSecurityConfig.java
+++ securing_web/src/main/java/hello/WebSecurityConfig.java
@@ -10,6 +10,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        
+        http
+            .authorizeRequests()
+                .antMatchers("/","/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
     }
 }
