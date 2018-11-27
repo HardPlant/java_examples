71c54ab061719d2c9bf5948a88791ac39dd10c69
HardPlant <abc7988se@naver.com>
2018-11-27 15:07:26 +0900

    jni?

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-groupId=com.seongwon.cipher
-artifactId=simple-cipher
+groupId=com.seongwon.jni
+artifactId=java-jni
 archetypeArtifactId="maven-archetype-quickstart"
 interactiveMode="false"
 module=App
---
+++ java-jni/src/main/java/com/seongwon/jni/App.java
@@ -0,0 +1,13 @@
+package com.seongwon.jni;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
