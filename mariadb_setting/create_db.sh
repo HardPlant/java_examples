0828525d66a098264b49786cfc7722a55f90924d
HardPlant <abc7988se@naver.com>
2018-11-27 10:41:25 +0900

    sql rename

--- mariadb_setting/create_db.sh
+++ mariadb_setting/create_db.sh
@@ -7,7 +7,7 @@ USERNAME=${4:-somedbuser}
 USERPASS=${5:-'USERPASS_HERE'}
 
 docker cp $(pwd)/config.sh $APP_NAME:/tmp/config.sh
-docker cp $(pwd)/create_db_sql.sh $APP_NAME:/tmp/create_db_sql.sh
+docker cp $(pwd)/_sql_create_db.sh $APP_NAME:/tmp/_sql_create_db.sh
 docker exec $APP_NAME /bin/bash -c /tmp/create_db_sql.sh
 #docker exec $APP_NAME /bin/bash < $(pwd)/create_db_sql.sh $1 $2 $3 $4 $5
 docker exec -it $APP_NAME mysql -uroot -p$PASSWD
