649e2c2e176c8c31a91d1a707fe38536e46c5da3
HardPlant <abc7988se@naver.com>
2018-11-27 11:03:27 +0900

    removed interaction

--- mariadb_setting/run_sql.sh
+++ mariadb_setting/run_sql.sh
@@ -7,4 +7,3 @@ docker cp $(pwd)/_sql_sql_run.sh $APP_NAME:/tmp/_sql_sql_run.sh
 docker cp $(pwd)/$SQL_NAME $APP_NAME:/tmp/schema.sql
 
 docker exec $APP_NAME /bin/bash -c /tmp/_sql_sql_run.sh
-docker exec -it $APP_NAME mysql -uroot -p$PASSWD
