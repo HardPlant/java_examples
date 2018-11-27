5ce47bf44acf2c589ff679e1b695a15e028230ce
HardPlant <abc7988se@naver.com>
2018-11-27 11:02:28 +0900

    removed interact

--- mariadb_setting/check_db.sh
+++ mariadb_setting/check_db.sh
@@ -3,4 +3,3 @@ source $(pwd)/config.sh
 docker cp $(pwd)/config.sh $APP_NAME:/tmp/config.sh
 docker cp $(pwd)/_sql_check_db.sh $APP_NAME:/tmp/_sql_check_db.sh
 docker exec $APP_NAME /bin/bash -c /tmp/_sql_check_db.sh
-docker exec -it $APP_NAME mysql -uroot -p$PASSWD
