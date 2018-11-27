180f8f62f64148b7bac80f3cea0c8d1d17aee820
HardPlant <abc7988se@naver.com>
2018-11-27 12:39:43 +0900

    make it runnable

--- spring_mysql/src/main/java/hello/UserRepository.java
+++ spring_mysql/src/main/java/hello/UserRepository.java
@@ -6,6 +6,6 @@ import hello.User;
 // 스프링에 의해 자동 구현됨
 // userRepository 빈으로 구현되며, CRUD함
 
-public interface UserRepositoy extends CrudRepository<User, Integer>{
+public interface UserRepository extends CrudRepository<User, Integer>{
 
 }
