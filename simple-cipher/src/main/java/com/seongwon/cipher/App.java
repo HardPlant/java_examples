835540cd9a3441bf18aaef2e23466e458e970441
HardPlant <abc7988se@naver.com>
2018-11-27 13:47:58 +0900

    okay, it works.

--- simple-cipher/src/main/java/com/seongwon/cipher/App.java
+++ simple-cipher/src/main/java/com/seongwon/cipher/App.java
@@ -7,6 +7,7 @@ import java.nio.charset.CharsetEncoder;
 import java.security.SecureRandom;
 
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
@@ -19,23 +20,32 @@ public class App
 {
     public static void main( String[] args ) throws Throwable
     {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
         SecureRandom secureRandom = new SecureRandom();
-        byte[] key = new byte[16];
-        secureRandom.nextBytes(key);
-        SecretKey secretKey = SecretKeySpec();
+        int keyBitSize = 128;
+        keyGenerator.init(keyBitSize, secureRandom);
+        SecretKey secretKey = keyGenerator.generateKey();
 
         byte[] iv = new byte[12];
         secureRandom.nextBytes(iv);
 
-        final Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
         GCMParameterSpec parameterSpec = new GCMParameterSpec(128, iv);
         cipher.init(Cipher.ENCRYPT_MODE, secretKey, parameterSpec);
 
         byte[] plainText = "abcabcaabc".getBytes("UTF-8");
         byte[] cipherText = cipher.doFinal(plainText);
 
-        for(int i=0; i<cipherMessage.length; i++){
-            System.out.println(Integer.toHexString(cipherMessage[i]));
+        for(int i=0; i<cipherText.length; i++){
+            System.out.print(Integer.toHexString(cipherText[i]) + " ");
         }
+        System.out.println();
+
+
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, parameterSpec);
+
+        
+        byte[] decipher = cipher.doFinal(cipherText);
+        System.out.println(new String(decipher, "UTF-8").trim());
     }
 }
