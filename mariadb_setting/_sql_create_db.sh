80d1bbcd273dbe0d980bfc96cb7c411d45d35a51
HardPlant <abc7988se@naver.com>
2018-11-27 10:46:09 +0900

    it works

--- mariadb_setting/_sql_create_db.sh
+++ mariadb_setting/_sql_create_db.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 source /tmp/config.sh
 
-mysql -uroot -p$PASSWD -e "
-create database $DBNAME;s
-grant all privileges on $DBNAME.* to $USERNAME identified by '$USERPASS';
-flush privileges;
-"
+
+mysql -uroot -p$PASSWD -e "drop database $DBNAME;"
+mysql -uroot -p$PASSWD -e "drop user $USERNAME;"
+
+mysql -uroot -p$PASSWD -e "create database $DBNAME;"
+mysql -uroot -p$PASSWD -e "grant all privileges on $DBNAME.* to $USERNAME identified by '$USERPASS';"
+mysql -uroot -p$PASSWD -e "flush privileges;"
