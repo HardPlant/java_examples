0d95f87915fc37c7b100d37090dea7880ea31a1f
HardPlant <abc7988se@naver.com>
2018-11-27 11:10:21 +0900

    works for custom sql

---
+++ mariadb_setting/schema.1.sql
@@ -0,0 +1,17 @@
+use somedb;
+
+DROP TABLE IF EXISTS shop;
+DROP TABLE IF EXISTS shop2;
+
+CREATE TABLE shop2 (
+    article INT(4) UNSIGNED ZEROFILL DEFAULT '0000' NOT NULL,
+    dealer  CHAR(20)                 DEFAULT ''     NOT NULL,
+    price   DOUBLE(16,2)             DEFAULT '0.00' NOT NULL,
+    PRIMARY KEY(article, dealer));
+
+INSERT INTO shop2 VALUES
+    (1,'A',3.45),(1,'B',3.99),(2,'A',10.99),(3,'B',1.45),
+    (3,'C',1.69),(3,'D',1.25),(4,'D',19.95);
+
+SELECT * FROM shop2 ORDER BY article;
+DROP TABLE shop2;
