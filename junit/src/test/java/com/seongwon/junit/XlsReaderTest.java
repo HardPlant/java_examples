abe84bc4bab250f3e9b5ac555dc65c6aa05d3cb9
HardPlant <abc7988se@naver.com>
2018-11-29 15:19:54 +0900

    it works

--- junit/src/test/java/com/seongwon/junit/XlsReaderTest.java
+++ junit/src/test/java/com/seongwon/junit/XlsReaderTest.java
@@ -3,13 +3,16 @@ package com.seongwon.junit;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.net.URL;
 
 import org.junit.Test;
 
 public class XlsReaderTest{
     @Test
     public void setFileTest() throws Throwable{
-        XlsReader reader = new XlsReader(new File("src/resources/data.xlsx"));
+        URL url = this.getClass().getClassLoader().getResource("data.xlsx");
+
+        XlsReader reader = new XlsReader(new File(url.toURI()));
         assertTrue(reader.getFile() != null);
     }
 }
