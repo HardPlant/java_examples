afde7549569458093ab42dc26d6a991783a6ffb2
HardPlant <abc7988se@naver.com>
2018-12-01 23:46:27 +0900

    a Expression Object

---
+++ calc/src/main/java/com/seongwon/calc/Expression.java
@@ -0,0 +1,12 @@
+package com.seongwon.calc;
+
+public class Expression{
+    Operand operand;
+    public Expression(String operand, int a, int b){
+
+    }
+    
+    public Operand getOperandObject() {
+        return operand;
+    }
+}
