76e2c69e1f502f22c0f4708c02284087d78cea20
HardPlant <abc7988se@naver.com>
2018-12-01 23:50:16 +0900

    now expr will parse int

--- calc/src/main/java/com/seongwon/calc/Calculator.java
+++ calc/src/main/java/com/seongwon/calc/Calculator.java
@@ -7,20 +7,6 @@ public class Calculator{
     public Calculator(){
     }
 
-    public int findNumberInExpr(){
-        int start = 0;
-        int end = 0;
-        for(int i=0; i<expr.length();i++){
-            int current = expr.charAt(i);
-            if('0'<=current && current <='9'){
-                continue;
-            } else{
-                end = i;
-                break;
-            }
-        }
-        return Integer.valueOf(expr.substring(start,end));
-    }
     
 
     /**
--- calc/src/main/java/com/seongwon/calc/Expression.java
+++ calc/src/main/java/com/seongwon/calc/Expression.java
@@ -2,15 +2,33 @@ package com.seongwon.calc;
 
 public class Expression{
     Operand operand;
-    public Expression(String operand, int a, int b){
-
+    String expr;
+    public Expression(String expr){
+        this.expr = expr;
     }
 
     public Operand getOperandObject() {
         return operand;
     }
 
+    public void evaluate(){
 
+    }
+
+    public int findNumberInExpr(){
+        int start = 0;
+        int end = 0;
+        for(int i=0; i<expr.length();i++){
+            int current = expr.charAt(i);
+            if('0'<=current && current <='9'){
+                continue;
+            } else{
+                end = i;
+                break;
+            }
+        }
+        return Integer.valueOf(expr.substring(start,end));
+    }
     public double evaluate(String operand, String first, String second){
         Integer operand1 = Integer.valueOf(first);
         Integer operand2 = Integer.valueOf(second);
