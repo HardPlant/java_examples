1d87c301b411150288fccd8cd5921dd753a5ab64
HardPlant <abc7988se@naver.com>
2018-12-01 23:52:47 +0900

    this will not correct..

--- calc/src/main/java/com/seongwon/calc/Calculator.java
+++ calc/src/main/java/com/seongwon/calc/Calculator.java
@@ -7,7 +7,30 @@ public class Calculator{
     public Calculator(){
     }
 
-    
+    public int findParentheseExpr(){
+        int start = 0;
+        int end = 0;
+        for(int i=0; i<expr.length();i++){
+            int current = expr.charAt(i);
+            if(current != '('){
+                continue;
+            } else{
+                start = i;
+                break;
+            }
+        }
+        for(int i=start; i<expr.length();i++){
+            int current = expr.charAt(i);
+            if(current != ')'){
+                continue;
+            } else{
+                end = i;
+                break;
+            }
+        }
+
+        return new Expression(expr.substring(start,end));
+    }
 
     /**
      * @param expr the expr to set
