788082256ff1bd83b56d000947f80a547964b0b7
HardPlant <abc7988se@naver.com>
2018-12-02 23:58:25 +0900

    a skeleton

--- collection/src/main/java/com/seongwon/collection/BinarySearch.java
+++ collection/src/main/java/com/seongwon/collection/BinarySearch.java
@@ -1,5 +1,15 @@
 package com.seongwon.collection;
 
-public class BinarySearch{
+import com.seongwon.collection.Search;
+
+public class BinarySearch implements Search{
+    int[] array;
     
+    public BinarySearch(int[] array){
+        this.array = array;
+    }
+    @Override
+    public int search() {
+        return 0;
+    }
 }
