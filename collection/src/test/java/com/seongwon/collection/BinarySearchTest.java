09888729afeac5c74fc314cf1c884d924a1e15ea
HardPlant <abc7988se@naver.com>
2018-12-03 00:27:58 +0900

    succeses, 1.389s.

--- collection/src/test/java/com/seongwon/collection/BinarySearchTest.java
+++ collection/src/test/java/com/seongwon/collection/BinarySearchTest.java
@@ -13,15 +13,15 @@ import org.junit.Test;
 public class BinarySearchTest{
     @Test
     public void recursiveTest(){
-        int[] array = {Integer.MIN_VALUE, -1,0,1,9,4,7,13,26,42,   Integer.MAX_VALUE};
+        int[] array = {Integer.MIN_VALUE, -1,0,1,4,7,9,13,26,42,   Integer.MAX_VALUE};
         BinarySearch search = new BinarySearch(array);
         assertEquals(search.search(Integer.MIN_VALUE), 0);
         assertEquals(search.search(-1), 1);
         assertEquals(search.search(0), 2);
         assertEquals(search.search(1), 3);
-        assertEquals(search.search(9), 4);
-        assertEquals(search.search(4), 5);
-        assertEquals(search.search(7), 6);
+        assertEquals(search.search(4), 4);
+        assertEquals(search.search(7), 5);
+        assertEquals(search.search(9), 6);
         assertEquals(search.search(13), 7);
         assertEquals(search.search(26), 8);
         assertEquals(search.search(42), 9);
