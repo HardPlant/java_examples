b21a727ea1934fc452ed5cc31c8b133f0de664f5
HardPlant <abc7988se@naver.com>
2018-12-03 00:57:30 +0900

    a very first work.

--- collection/src/main/java/com/seongwon/collection/MergeSort.java
+++ collection/src/main/java/com/seongwon/collection/MergeSort.java
@@ -8,7 +8,19 @@ public class MergeSort implements Sort{
         this.array = array;
     }
     @Override
-    public int[] sort(int[] array) {
-        return null;
+    public int[] sort() {
+        
+        return this.array;
+    }
+    private void divide(int low, int high){
+        if(low>high) return;
+        int mid = (low + high) / 2;
+        divide(low, mid);
+        divide(mid+1, high);
+        conquer(low, high);
+    }
+    private void conquer(int low, int high){
+        for(int i=low; low<high; i++)
+            if(array[low] > array[high]) array[low] = array[high];
     }
 }
