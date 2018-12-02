9cc16a32c4dddaede5c6ccc698a16cd4d593e68f
HardPlant <abc7988se@naver.com>
2018-12-03 01:52:38 +0900

    this fixes arrayboundexception

--- collection/src/main/java/com/seongwon/collection/MergeSort.java
+++ collection/src/main/java/com/seongwon/collection/MergeSort.java
@@ -15,9 +15,12 @@ public class MergeSort implements Sort{
     }
     public int[] make_array(int[] array, int low, int high){
         int[] result = new int[high-low + 1];
+        System.out.println("Array Made:"); 
         for(int i=0; i<high-low+1; i++){
             result[i] = array[low+i];
+            System.out.print(result[i] + " "); 
         }
+        System.out.println();
         return result;
     }
     private void mergeSort_first(int[] array){
@@ -25,8 +28,8 @@ public class MergeSort implements Sort{
         if(n > 1){
             final int low_half = n/2;
             final int high_half = n - low_half;
-            int[] low_array = make_array(array, 0, low_half);
-            int[] high_array = make_array(array, low_half+1, high_half);
+            int[] low_array = make_array(array, 0, low_half-1);
+            int[] high_array = make_array(array, low_half, n);
             mergeSort_first(low_array);
             mergeSort_first(high_array);
             merge(low_half, high_half, low_array, high_array, array);
@@ -49,11 +52,11 @@ public class MergeSort implements Sort{
             k++;
         }
         if(j<=high_half){
-            for(;i < low_half+high_half;i++){
+            for(;i < low_half+high_half -1;i++){
                 merging_array[k] = low_array[i];
             }
         } else{
-            for(;j < low_half+high_half;j++){
+            for(;j < low_half+high_half -1;j++){
                 merging_array[k] = high_array[j];
             }
         }
