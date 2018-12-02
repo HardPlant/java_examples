f89eecdd1e47e76c5b6d31c062bfd310d493db69
HardPlant <abc7988se@naver.com>
2018-12-03 01:37:43 +0900

    now it uses mergetSort_first

--- collection/src/main/java/com/seongwon/collection/MergeSort.java
+++ collection/src/main/java/com/seongwon/collection/MergeSort.java
@@ -9,25 +9,8 @@ public class MergeSort implements Sort{
     }
     @Override
     public int[] sort(int[] array) {
-        setArray(array);
-        divide(0, array.length-1);
+        mergeSort_first(this.array);
         return this.array;
-    }
-    private void divide(int low, int high){
-        if(low>high) return;
-        int mid = (low + high) / 2;
-        divide(low, mid);
-        divide(mid+1, high);
-        conquer(low, high);
-    }
-    private void conquer(int low, int high){
-        for(int i=low; low<high; i++){
-            if(array[low] > array[high]){
-                int temp = array[low];
-                array[low] = array[high];
-                array[high] = temp;
-            }
-        }
     }
     public int[] make_array(int[] array, int low, int high){
         int[] result = new int[high-low + 1];
