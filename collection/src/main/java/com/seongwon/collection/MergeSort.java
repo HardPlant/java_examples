fb4d87d16c6b0ab407569366b0c8e26cc32646ea
HardPlant <abc7988se@naver.com>
2018-12-03 00:46:53 +0900

    a mergesort class

---
+++ collection/src/main/java/com/seongwon/collection/MergeSort.java
@@ -0,0 +1,14 @@
+package com.seongwon.collection;
+
+import com.seongwon.collection.Sort;
+
+public class MergeSort implements Sort{
+    int[] array;
+    public MergeSort(int[] array){
+        this.array = array;
+    }
+    @Override
+    public int[] sort(int[] array) {
+        return null;
+    }
+}
