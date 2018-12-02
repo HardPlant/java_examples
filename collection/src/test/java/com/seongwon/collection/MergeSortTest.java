a2da24ac646ac3b01899102182d62bb172f722ea
HardPlant <abc7988se@naver.com>
2018-12-03 01:00:54 +0900

    typo

--- collection/src/test/java/com/seongwon/collection/MergeSortTest.java
+++ collection/src/test/java/com/seongwon/collection/MergeSortTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 public class MergeSortTest{
-    private bollean is_sorted(int[] array){
+    private boolean is_sorted(int[] array){
         for(int idx = 0; idx < array.length-1; idx++){
             if(array[idx]>array[idx+1]) return false;
         }
